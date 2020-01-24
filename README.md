525f91ef6b29621cf6ad37728eb134be522c6ddf
HasheebKazi <hasheeb.k@gmail.com>
2020-01-24 12:42:53 -0500

    new routes template

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# image_repository
+# Stock Image Repository Web App
 
-This is an stock image selling website similar to [https://stock.adobe.com](https://stock.adobe.com/). 
+This is an stock image selling website similar to [https://stock.adobe.com](https://stock.adobe.com/). The project is created using React, NodeJs, Express and Firebase.
 
 ## info
 This app was created with React for the front-end and NodeJs and Express in the backend. Currently Hosting and DB is provided by Firebase.
--- backend/routes/formRoutes.js
+++ backend/routes/formRoutes.js
@@ -7,16 +7,20 @@ const { body } = require('express-validator');
 // internal
 const form_controllers = require('../controllers/form_controllers');
 
+/** External Library Imports **/
+const router = require('express').Router();
+const { check, body } = require('express-validator');
 
-// constants
+/** Internal Dependencies **/
+const formController = require('../controllers/formControllers');
+
+/** Source Code **/
 const FILE_NAME = 'name';
 const TAGS = 'tags';
-
-// routes setup
-const router = express.Router();
 
 router.post('/', [
     body(FILE_NAME, 'DEFAULT ERROR MESSAGE').trim().isAlphanumeric()
 ], form_controllers.postUploads);
 
+/** Module Exports **/
 module.exports = router;
